fe73bcf767d3eae1e093af62097d24cf3fa528c4
Harald Glab-Plhak <hglabplhak@gmail.com>
2025-12-11 23:22:03 +0100

    renamed package to io.github.hglabplh_tech.games and moved class Main

--- src/main/java/io/github/hglabplh_tech/games/Main.java
+++ src/main/java/io/github/hglabplh_tech/games/Main.java
@@ -1,4 +1,4 @@
-package io.examples.sort;
+package io.github.hglabplh_tech.games;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
